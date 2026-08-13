51e277698aa665068ef272fbc5c999b681855c37
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-08-14 00:50:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shizupet-safety-fix-v11';
+const CACHE='shizupet-facility-update-v11';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
