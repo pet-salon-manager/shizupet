cb7d32f453065c9d8e1da2982bc9fe9f9f8ef4e4
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-08-13 20:17:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shizupet-supabase-admin-v1';
+const CACHE='shizupet-supabase-connected-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
